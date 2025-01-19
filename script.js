76956669a293fcf0da19616577a6ff1bab8373b3
pipypupa <zong8729@gmail.com>
2025-01-19 17:24:48 +0200

    fix

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 // 1
-
-const article = document.querySelector('.article')
-const button = document.querySelector('button')
-button.textContent = article.textContent
+const article = document.querySelector(".article");
+const button = document.querySelector("button");
+button.textContent = article.textContent;
 
 // 2
 
-const image = document.querySelector('img')
-const title = document.querySelector('h2')
-image.src = './краб.jpeg'
-title.style.margin = '20px'
+const image = document.querySelector("img");
+const title = document.querySelector("h2");
+image.src = "./краб.jpeg";
+title.style.margin = "20px";
 // 3
 
-const image2 = document.querySelector('#image3')
-image3.alt = 'не заєць'
-const link = document.querySelector('a')
-link.href = 'https://zvidusil.in.ua/lepus-timidus/'
+const image2 = document.querySelector("#image3");
+image3.alt = "не заєць";
+const link = document.querySelector("a");
+link.href = "https://zvidusil.in.ua/lepus-timidus/";
 
 // 4
 
-const list = document.querySelector('ul')
-const firstChild = document.querySelector('li.firstElement')
-firstChild.textContent = 'замінений текст для li'
-list.style.border = 'solid black'
+const list = document.querySelector("ul");
+const firstChild = document.querySelector("li.firstElement");
+firstChild.textContent = "замінений текст для li";
+list.style.border = "solid black";
