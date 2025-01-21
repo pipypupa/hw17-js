db4fa1c32005e6d7b9e235f2eb6fada660146ad7
pipypupa <zong8729@gmail.com>
2025-01-21 03:29:18 +0200

    fix name and width

--- script.js
+++ script.js
@@ -4,20 +4,18 @@ const button = document.querySelector("button");
 button.textContent = article.textContent;
 
 // 2
-
 const image = document.querySelector("img");
 const title = document.querySelector("h2");
-image.src = "./краб.jpeg";
+image.src = "./image2.jpeg";
 title.style.margin = "20px";
-// 3
 
+// 3
 const image2 = document.querySelector("#image3");
 image3.alt = "не заєць";
 const link = document.querySelector("a");
 link.href = "https://zvidusil.in.ua/lepus-timidus/";
 
 // 4
-
 const list = document.querySelector("ul");
 const firstChild = document.querySelector("li.firstElement");
 firstChild.textContent = "замінений текст для li";
